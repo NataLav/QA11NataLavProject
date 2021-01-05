aef735d059fcc1954ca5aee7c9f7a75bbcb9a007
NataLav <pismosuda@mail.ru>
2021-01-05 21:00:03 +0100

    Created project

---
+++ src/main/java/com/telran/qa11/FirstSeleniumTest.java
@@ -0,0 +1,27 @@
+package com.telran.qa11;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class FirstSeleniumTest {
+    WebDriver wd;
+    //befor - setUp(){}
+
+    @BeforeMethod
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "/Users/natalialavrynenko/Desktop/ToolsQA/chromedriver");
+        wd = new ChromeDriver();
+        wd.get("https://www.google.com/");
+    }
+
+    //Test
+
+    @Test
+    public void openGoogleDrive(){
+        System.out.println("site opened");
+    }
+
+    //after -tearDown(){{} закрывает
+}
